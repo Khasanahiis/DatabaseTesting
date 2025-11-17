7239955493ee4507ce30db75195fd7b9f351e30d
Iis Naeni Khasanah <khasanahnaenis@gmail.com>
2025-11-17 11:04:38 +0700

    database testing

---
+++ src/main/java/com/praktikum/database/testing/library/model/User.java
@@ -0,0 +1,28 @@
+package com.praktikum.database.testing.library.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.sql.Timestamp;
+
+/**
+ * Entity class yang merepresentasikan tabel 'users' dalam database
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer userId;
+    private String username;
+    private String email;
+    private String fullName;
+    private String phone;
+    private String role;
+    private String status;
+    private Timestamp registrationDate;
+    private Timestamp lastLogin;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+}
---
+++ src/test/java/com/praktikum/database/testing/library/library/utils/BaseDatabaseTest.java
@@ -0,0 +1,4 @@
+package com.praktikum.database.testing.library.library.utils;
+
+public class BaseDatabaseTest {
+}
